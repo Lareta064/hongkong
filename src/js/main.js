6778c983ed43e4e70bb546b532f1ece89b1858d7
Lareta <lareta064@gmail.com>
2020-05-08 19:19:03 +0300

    full

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,26 @@
 $(document).ready(function () {
+	$("#page-nav").onePageNav({
+		currentClass: "active",
+		changeHash: false,
+		scrollSpeed: 750,
+		scrollThreshold: 0.5,
+		filter: "",
+		easing: "swing",
+		begin: function () {},
+		end: function () {},
+		scrollChange: function ($currentListItem) {}
+	});
+	$(".header-nav").onePageNav({
+		currentClass: "active",
+		changeHash: false,
+		scrollSpeed: 750,
+		scrollThreshold: 0.5,
+		filter: "",
+		easing: "swing",
+		begin: function () {},
+		end: function () {},
+		scrollChange: function ($currentListItem) {}
+	});
 
 
 })
